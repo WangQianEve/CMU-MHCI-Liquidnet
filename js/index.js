d60b7b78fb5ec830f68474e871bb3ad44b720b9f
Qian <eli_home@outlook.com>
2019-07-30 08:51:45 -0400

    putting things in

--- js/index.js
+++ js/index.js
@@ -6,3 +6,12 @@ function expandNav(elem) {
         nav_section.addClass('opened');
     }
 }
+
+function expandRef(elem) {
+    let nav_section = $(elem).parents('.references');
+    if (nav_section.hasClass('opened')) {
+        nav_section.removeClass('opened');
+    } else {
+        nav_section.addClass('opened');
+    }
+}
